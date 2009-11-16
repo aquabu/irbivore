d3d1294c943027cc8ebf8d17abc2395e0760c95c
Noah Thorp <noahthorp@rix-10.gateway.2wire.net>
2009-11-15 22:24:18 -0800

    Irbivore messaging on irb startup.

--- irbivore.rb
+++ irbivore.rb
@@ -5,6 +5,9 @@ require 'irb/completion'
 require 'boson'
 require File.expand_path(File.dirname(__FILE__)) + '/init.rb'
 # include Irbivore::Livecoding
+puts "\n\n\n"
+puts "Irbivore welcomes you"
+puts "Commands: midiator_start, play"
 Boson.start :libraries => [Irbivore::Livecoding]
 Irbivore::Livecoding.setup
 IRB.start
