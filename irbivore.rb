11cbcb9835a6f2b78cb4efd75288a7448941ff73
Noah Thorp <noahthorp@rix-10.gateway.2wire.net>
2009-11-18 13:51:28 -0800

    Added irbivore config file options.

--- irbivore.rb
+++ irbivore.rb
@@ -4,10 +4,17 @@ require 'irb'
 require 'irb/completion'
 require 'boson'
 require File.expand_path(File.dirname(__FILE__)) + '/init.rb'
-# include Irbivore::Livecoding
+
+if ARGV[0]
+  require ARGV[0];
+  ARGV[0] = nil
+else
+  require 'config.rb'
+end
+
 puts "\n\n\n"
 puts "Irbivore welcomes you"
 puts "Commands: midiator_start, play"
-Boson.start :libraries => [Irbivore::Livecoding], :verbose => false
+Boson.start :libraries => [Irbivore::Livecoding, Irbivore::Config], :verbose => false
 Irbivore::Livecoding.setup
 IRB.start
